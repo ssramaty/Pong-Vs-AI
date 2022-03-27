bb39f9676b5dfb13cb945c9497a106696d94b277
ssramaty <102402310+ssramaty@users.noreply.github.com>
2022-03-26 21:17:26 -0400

    test

--- ai.py
+++ ai.py
@@ -1,1 +1,5 @@
 import pygame
+
+cpu_img = pygame.image.load('player.png')
+cpu_X = width - 10 - 27
+cpu_Y = height/2 - 100
--- player.py
+++ player.py
@@ -1,1 +1,6 @@
 import pygame
+
+player_img = pygame.image.load('player.png')
+player_X = 10
+player_Y_default = height / 2 - 100
+player_Y_dif = 0
